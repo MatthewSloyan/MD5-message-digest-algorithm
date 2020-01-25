d06fccf0c440a44b2c8bf78265365515dd2cd529
Matthew Sloyan <g00348036@gmit.ie>
2020-01-25 16:17:28 +0000

    Setup project.

---
+++ MD5-message-digest-algorithm/main.c
@@ -0,0 +1,18 @@
+// C program that calculates the MD5 hash digest of an input - 4th year Theory of Algorithms
+// by Matthew Sloyan G00348036
+
+//Github Link:
+//https://github.com/MatthewSloyan/MD5-message-digest-algorithm
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+
+//global variables
+FILE* filep;
+
+void main()
+{
+	
+}
