a2e6d8735882a8a49539ccd5fa4b72e99d132fc8
Matthew Sloyan <g00348036@gmit.ie>
2020-01-25 16:31:46 +0000

    Setup basic menu system with initial and subsequent read.

--- MD5-message-digest-algorithm/main.c
+++ MD5-message-digest-algorithm/main.c
@@ -9,10 +9,41 @@
 #include<string.h>
 #include<conio.h>
 
-//global variables
-FILE* filep;
-
 void main()
 {
-	
+	FILE* filep;
+	int userSelection = 0;
+
+	printf("MD5 Hash Algorithm\n");
+	printf("===============\n");
+
+	//allow the user to enter a selection from the menu (intitial read)
+	do
+	{
+		printf("\nPlease select an option\n [1] Enter text to hash\n [2] Hash with file\n");
+		printf(" [0] To Exit\n");
+		scanf_s("%d", &userSelection);
+
+		if (userSelection < 0 || userSelection > 3) {
+			printf("Error! The value entered must be between 0 and 2, please try again\n");
+		}
+
+	} while (userSelection < 0 || userSelection > 3); //validation to allow only numbers between 0 and 2
+
+	// main while loop of program until 0 is encountered
+	while (userSelection != 0)
+	{
+		//subsequent Read
+		do
+		{
+			printf("\nPlease select an option\n [1] Enter text to hash\n [2] Hash with file\n");
+			printf(" [0] To Exit\n");
+			scanf_s("%d", &userSelection);
+
+			if (userSelection < 0 || userSelection > 3) {
+				printf("Error! The value entered must be between 0 and 2, please try again\n");
+			}
+
+		} while (userSelection < 0 || userSelection > 3); //validation to allow only numbers between 0 and 2
+	} //while
 }
