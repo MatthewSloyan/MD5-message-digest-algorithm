73e66fe5fefdfb6746fe3db8805526d01534d229
MatthewSloyan <G00348036@gmit.ie>
2020-02-04 15:13:55 +0000

    Test C code to learn more about bits.

---
+++ TestCode/lookatbits.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>>
+
+void printBinary();
+
+int main(int argc, char *argv[]){
+  
+  //char c = 65;
+  //int i = 65;
+  //long int l = 65;
+  //long long int m = 65;
+
+  uint8_t e = 65;
+  uint64_t s = 65;
+
+  // sizeof() print outs bytes.
+  //printf("%x\t%c\t%d\t%d\n",c, c, c, sizeof(c));
+  //printf("%x\t%c\t%d\t%d\n",i, i, i, sizeof(i);
+  //printf"%x\t%c\t%d\t%d\n",l, l, l, sizeof(l);
+
+  printf"%x\t%c\t%d\t%d\n",m, m, m, sizeof(m);
+
+  return 0;	
+}
