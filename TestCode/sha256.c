7388d24e80489c8f2e51ffd13d12c0291fd85d01
Matthew Sloyan <g00348036@gmit.ie>
2020-02-05 22:03:50 +0000

    Added Sig0, Sig1, sig0, sig1 bit rotation functions to algorithm.

--- TestCode/sha256.c
+++ TestCode/sha256.c
@@ -25,6 +25,23 @@ uint32_t ROTR(uint32_t x, int n){
     return (x >> n) | (x << (32 - n));
 }
 
+// One way bit rotation functions described in Section 4.1.2
+uint32_t Sig0(uint32_t x){
+    return ROTR(x, 2) ^ ROTR(x, 13) ^ ROTR(x, 22);
+}
+
+uint32_t Sig1(uint32_t x){
+    return ROTR(x, 6) ^ ROTR(x, 11) ^ ROTR(x, 25);
+}
+
+uint32_t sig0(uint32_t x){
+    return ROTR(x, 7) ^ ROTR(x, 18) ^ SHR(x, 3);
+}
+
+uint32_t sig1(uint32_t x){
+    return ROTR(x, 17) ^ ROTR(x, 19) ^ SHR(x, 10);
+}
+
 int main(int argc, char *argv[]){
 
     uint32_t x = 0x0f0f0f0f;
@@ -38,6 +55,14 @@ int main(int argc, char *argv[]){
 
     printf("Ch(x, y, z) = %08x\n", Ch(x, y, z));
     printf("Maj(x, y, z)= %08x\n", Maj(x, y, z));
+
+    printf("SHR(x, 4)   = %08x\n", SHR(x, 4));
+    printf("ROTR(x, 4)  = %08x\n", ROTR(x, 4));
+
+    printf("Sig0(x)  = %08x\n", Sig0(x));
+    printf("Sig1(x)  = %08x\n", Sig1(x));
+    printf("sig0(x)  = %08x\n", sig0(x));
+    printf("sig0(x)  = %08x\n", sig0(x));
 
     return 0;
 }
