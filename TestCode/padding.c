2732a5f46bad861ed184bb412f460c89d654c2b8
Matthew Sloyan <g00348036@gmit.ie>
2020-02-11 22:28:12 +0000

    Add 1 bit to message and started nozerobytes method to count number of zeros to add.

--- TestCode/padding.c
+++ TestCode/padding.c
@@ -4,6 +4,15 @@
 #include<stdio.h>
 #include<inttypes.h>
 
+// k = Number of zeros to append.
+uint64_t nozerobytes(uint64_t nobits) {
+
+    //ULL = unsigned long long. Tells C compiler it should be a 64 bit int.
+    uint64_t result = 512ULL - (nobits % 512ULL);
+
+    return 0
+}
+
 int main(int argc, char *argv[]){
 
     if (argc != 2){
@@ -26,6 +35,10 @@ int main(int argc, char *argv[]){
     for(nobits = 0; fread(&b, 1, 1, infile == 1; nobits += 8) {
         printf("%02" PRIx8, b);
     }
+
+    // Add 1 bit
+    // Bits: 1000 0000 = 1 bit followed by 7 zeros to pad. Always has to be 8bits.
+    printf("%02" PRIx8, 0x80); 
 
     fclose(infile);
 
