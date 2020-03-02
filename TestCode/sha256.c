37787e6cdd18c1c597b8acfd1eb605ad46665599
Matthew Sloyan <g00348036@gmit.ie>
2020-03-02 16:28:03 +0000

    Added steps 3 & 4 to hashing function from video.

--- TestCode/sha256.c
+++ TestCode/sha256.c
@@ -125,7 +125,7 @@ void nexthash(union block *M, uint32_t *H){
     
     // Section 6.2.2
     uint32_t W[64];
-    uint32_t a, b, c, d, e, f, g, h;
+    uint32_t a, b, c, d, e, f, g, h, T1, T2;
     int t;
 
     for (t = 0; t < 16; t++){
@@ -138,6 +138,16 @@ void nexthash(union block *M, uint32_t *H){
 
     a = H[0]; b = H[1]; c = H[2]; d = H[3];
     e = H[4]; f = H[5]; g = H[6]; h = H[7]; 
+
+    for (t = 0; t < 64; t++){
+        T1 = h + Sig1(e) + Ch(e, f, g) + K[t] + W[t];
+        T2 = Sig1(e) + Maj(a, b, c);
+        h = g; g = f; f = e; e = d + T1;
+        d = c; c = b; b = a; a = T1 + T2;
+
+        H[0] = a + H[0]; H[1] = b + H[1]; H[2] = c + H[2]; H[3] = d + H[3];
+        H[4] = e + H[4]; H[5] = f + H[5]; H[6] = g + H[6]; H[7] = h + H[7]; 
+    }
 }
 
 int main(int argc, char *argv[]){
