492da5cec0a72ae51db888ae23cb106e752f9a90
Matthew Sloyan <g00348036@gmit.ie>
2020-03-15 14:32:45 +0000

    Added predefined hash values and made changes to initial buffer values.

--- main.c
+++ main.c
@@ -13,10 +13,25 @@
 // Preprocessor
 #define WORD uint32_t
 
-// These 32 bit registers are initialized to the following values in hexadecimal, low-order bytes first.
-//const uint32_t digest[] = { 0x01234567, 0x89abcdef, 0xfedcba98, 0x76543210 };
-// These 32 bit registers are initialized to the following values in hexadecimal, high-order bytes first.
-const uint32_t initialValues[] = { 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476 };
+// Predifined hash values, defined in 
+const uint32_t T[64] = {
+    0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
+    0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
+    0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
+    0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
+    0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
+    0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
+    0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
+    0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
+    0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
+    0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
+    0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
+    0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
+    0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
+    0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
+    0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
+    0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
+};
 
 // Pad0 = get to eof, don't have enough space to do all padding. Pad rest with 0's
 // pad1 = read to end block perfectly, E.g 512 bits + padding. 1 bit + 450 0 bits.
@@ -52,11 +67,10 @@ union block{
 // Push bits off to the left n places, however they are pushed in on the right again (loop around)
 #define ROTL((x << n) | (x >> (32 - n)))
 
-void nexthash(WORD *M){
+void nexthash(WORD *M, WORD *H){
 
 	// All steps to hash each 16 word block. 
 	// Adapted from documentation: https://tools.ietf.org/html/rfc1321
-    WORD W[64];
     WORD a, b, c, d;
     int i, j;
 
@@ -65,10 +79,10 @@ void nexthash(WORD *M){
     }
 
 	// Assign initial values to temp variables in memory.
-	a = initialValues[0];
-    b = initialValues[1];
-    c = initialValues[2];
-    d = initialValues[3];
+	a = H[0];
+    b = H[1];
+    c = H[2];
+    d = H[3];
 }
 
 // PAD the message
@@ -202,11 +216,16 @@ void main()
     uint64_t nobits = 0;
     enum flag status = READ;
 
+    // These 32 bit registers are initialized to the following values in hexadecimal, low-order bytes first.
+    //const uint32_t digest[] = { 0x01234567, 0x89abcdef, 0xfedcba98, 0x76543210 };
+    // These 32 bit registers are initialized to the following values in hexadecimal, high-order bytes first.
+    uint32_t H[] = { 0x67452301, 0xefcdab89, 0x98badcfe, 0x10325476 };
+
     // Read through all the padded message blocks.
     while (nextblock(&M, infile, &nobits, &status)){
       // Calculate the next hash value.
       // Pass memory address of M.
-      //nexthash(M.threetwo, H);
+      nexthash(M.threetwo, H);
     }
 
     for(int i = 0; i < 8; i++){
