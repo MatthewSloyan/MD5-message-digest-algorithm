86fb49bb6b5083026e123de6519389c7f2352266
Matthew Sloyan <g00348036@gmit.ie>
2020-04-17 19:30:31 +0100

    Started to add command line arguments using getopt.

--- main.c
+++ main.c
@@ -449,19 +449,43 @@ int main(int argc, char *argv[])
 
   // Check if there's input from the comand line,
   // as hashing can be completed quickly without menu UI.
-  if (argc == 2)
+  if (argc >= 2)
   {
-    infile = fopen(argv[1], "rb");
-
-    if (!infile)
-    {
-      printf("Error: couldn't open file %s. \n", argv[1]);
-      return 1;
+    int option;
+    // https://www.tutorialspoint.com/getopt-function-in-c-to-parse-command-line-arguments
+    // put ':' at the starting of the string so compiler can distinguish between '?' and ':'
+    while((option = getopt(argc, argv, ":if:lrx")) != -1){ //get option from the getopt() method
+      switch(option){
+        //For option i, r, l, print that these are options
+        case 'i':
+        case 'l':
+        case 'r':
+            printf("Given Option: %c\n", option);
+            break;
+        case 'f': //here f is used for some file name
+            printf("Given File: %s\n", optarg);
+            break;
+        case ':':
+            printf("option needs a value\n");
+            break;
+        case '?': //used for some unknown options
+            printf("unknown option: %c\n", optopt);
+            break;
+      }
     }
+    // infile = fopen(argv[1], "rb");
 
-    // Start hashing the file. 0 = file.
-    // Uses same function in menu also (DRY)
-    startMD5(infile, str, 0);
+    // if (!infile)
+    // {
+    //   printf("Error: couldn't open file %s. \n", argv[1]);
+    //   return 1;
+    // }
+    // else {
+    //   // Start hashing the file. 0 = file.
+    //   // Uses same function in menu also (DRY)
+    //   startMD5(infile, str, 0);
+    // }
+    
   }
   else
   {
